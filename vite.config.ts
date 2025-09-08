212795f9951887b326692b6bba050640fdf827ea
ohba-t0321 <to19830321@gmail.com>
2025-09-08 23:53:37 +0900

    自動ビルド準備

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "lawdata-app-vite",
   build: {
     outDir: "dist", // Viteデフォルト dist/
   },
