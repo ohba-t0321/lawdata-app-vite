f0e17f4f60b1a54c0836f44639fb131d2b4abaf1
ohba-t0321 <to19830321@gmail.com>
2025-09-10 00:08:28 +0900

    ref_link修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/lawdata-app-vite",
+  base: "/lawdata-app-vite/",
   build: {
     outDir: "dist", // Viteデフォルト dist/
   },
